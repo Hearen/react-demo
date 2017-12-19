a2da9c90bca0cb08cee10a735093f86d0b927d59
LHearen <lhearen@gmail.com>
2017-12-19 10:03:13 +0800

    original commit

---
+++ src/app/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+ 
+class SimpleComponent extends React.Component {
+    render(){
+        return (
+                <div>
+                {this.props.message}
+                </div>
+        );
+   } 
+}
+ 
+ReactDOM.render(
+    <SimpleComponent message="React Demo - test" />,
+    document.querySelector( '.js-app' )
+);
